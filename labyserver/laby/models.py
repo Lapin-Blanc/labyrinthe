e8535d818d2ef7b495cadb300933092d579a8ea0
Lapin-Blanc <github@lapin-blanc.com>
2018-10-11 15:12:33 +0200

    added multiple rounds for game

--- labyserver/laby/models.py
+++ labyserver/laby/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 
-class Games(models.Model):
-  player_one_code = models.TextField('Player one code', blank=True, null=True)
-  player_two_code = models.TextField('Player two code', blank=True, null=True)
-  last_connected = models.IntegerField('Last player connected', default=1)
+class Rounds(models.Model):
+  round_number = models.IntegerField('Round number', default=1)
+  player_one_code = models.TextField('Player one code', blank=True, default='')
+  p_one_submitted = models.BooleanField('Player one submitted code', default=False)
+  p_two_submitted = models.BooleanField('Player two submitted code', default=False)
+  player_two_code = models.TextField('Player two code', blank=True, default='')
+  last_active = models.IntegerField('Last player', default=0)
+  over = models.BooleanField('Round over', default=False)
+  
+  def __str__(self):
+    return "Round {}, last active {}, over = {}".format(
+      self.round_number,
+      self.last_active,
+      self.over
+    )
--- labyserver/laby/views.py
+++ labyserver/laby/views.py
@@ -1,48 +1,61 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Games
+from django.http import HttpResponse, JsonResponse
+from .models import Rounds
 from django.shortcuts import render
 
 
 def gameView(request):
-  d = {}  
-  game = Games.objects.get(pk=1)
-  if game.last_connected == 1:
-    game.last_connected = 0
-    game.player_one_code = "";
-    game.player_two_code = "";
-    game.save()
-    d['player'] = 0
-    d['code'] = game.player_one_code
-    
-  else:
-    game.last_connected = 1
-    game.save()
-    d['player'] = 1
-    d['code'] = game.player_two_code
-  
+  d = {}
+  current_round = Rounds.objects.filter(over__exact=False).last() # New round
+  if (not current_round):
+    current_round = Rounds()
+    d['player'] = current_round.last_active
+  else: # current round exists
+    current_round.last_active = (current_round.last_active + 1) % 2
+    d['player'] = current_round.last_active
+  current_round.save()
+  d['code'] = '';
+  d['round_number'] = current_round.round_number
   return render(request, 'index.html', d)
   
 
 def getCode(request):
   if request.method == "POST":
-    game = Games.objects.get(pk=1)
+    current_round = Rounds.objects.filter(round_number__exact=request.POST['round_number']).last()
     player = request.POST['player']
     code = request.POST['code']
-
     if player == '0' :
-      print(code)
-      game.player_one_code = code
-      game.save()
-      if game.player_two_code :
-        return HttpResponse(game.player_two_code, status=200)
+      current_round.player_one_code = code
+      current_round.p_one_submitted = True
+      current_round.save()
+      if current_round.p_two_submitted :
+        current_round.over = True
+        current_round.save()
+        new_round = Rounds(round_number = current_round.round_number + 1)
+        new_round.save()
+        response = JsonResponse({
+          'code': current_round.player_two_code,
+          'next_round' : new_round.round_number
+        }, status=200)
+        return response
       else :
-        return HttpResponse(status=202)
-    else :
-      print(code)
-      game.player_two_code = code
-      game.save()
-      if game.player_one_code :
-        return HttpResponse(game.player_one_code, status=200)
+        response = JsonResponse({'reason': 'waiting for player two\'s code'}, status=202)
+        return response
+    
+    if player == '1' :
+      current_round.player_two_code = code
+      current_round.p_two_submitted = True
+      current_round.save()
+      if current_round.p_one_submitted :
+        current_round.over = True
+        current_round.save()
+        new_round = Rounds(round_number = current_round.round_number + 1)
+        new_round.save()
+        response = JsonResponse({
+          'code': current_round.player_one_code,
+          'next_round' : new_round.round_number
+        }, status=200)
+        return response
       else :
-        return HttpResponse(status=202)
+        response = JsonResponse({'reason': 'waiting for player one\'s code'}, status=202)
+        return response
